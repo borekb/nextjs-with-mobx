b9c3dc5d4c7762f7eb1a71269db5a55f06992e37
Borek Bernard <borekb@gmail.com>
2019-08-10 16:34:28 +0200

    Add navigation between posts

--- pages/post/[id].js
+++ pages/post/[id].js
@@ -1,12 +1,15 @@
 import Link from 'next/link';
 
-const Post = ({ post }) => (
+const Post = ({ post, id }) => (
   <>
     <h1>Post</h1>
     <p>{post}</p>
     <div>
       <Link href='/'>
         <a>Home</a>
+      </Link>{' '}
+      <Link href='/post/[id]' as={`/post/${id === '1' ? '2' : '1'}`}>
+        <a>The other post</a>
       </Link>
     </div>
   </>
@@ -16,6 +19,7 @@ Post.getInitialProps = async ({ mobxStore: { postStore }, query }) => {
   await postStore.fetch(query.id);
   return {
     post: postStore.post,
+    id: query.id,
   };
 };
 
