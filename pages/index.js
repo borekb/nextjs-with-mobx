fe02d62d0a79b798f301d5c9a6c8d0585ced4df6
Borek Bernard <borekb@gmail.com>
2019-08-10 16:16:08 +0200

    Format with Prettier

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,16 @@ const Index = () => (
   <>
     <h1>MobX and Next.js example</h1>
     <ul>
-      <li><Link href="/post/[id]" as="/post/1"><a>Post 1</a></Link></li>
-      <li><Link href="/post/[id]" as="/post/2"><a>Post 2</a></Link></li>
+      <li>
+        <Link href='/post/[id]' as='/post/1'>
+          <a>Post 1</a>
+        </Link>
+      </li>
+      <li>
+        <Link href='/post/[id]' as='/post/2'>
+          <a>Post 2</a>
+        </Link>
+      </li>
     </ul>
   </>
 );
--- pages/post/[id].js
+++ pages/post/[id].js
@@ -4,15 +4,19 @@ const Post = ({ post }) => (
   <>
     <h1>Post</h1>
     <p>{post}</p>
-    <div><Link href="/"><a>Home</a></Link></div>
+    <div>
+      <Link href='/'>
+        <a>Home</a>
+      </Link>
+    </div>
   </>
 );
 
-Post.getInitialProps = async ({mobxStore: {postStore}, query}) => {
+Post.getInitialProps = async ({ mobxStore: { postStore }, query }) => {
   await postStore.fetch(query.id);
   return {
     post: postStore.post,
-  }
-}
+  };
+};
 
 export default Post;
